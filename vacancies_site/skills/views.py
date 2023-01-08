fb80524e0fad94b3b9e7ffe9088de5f04617c7b0
stillouyng <sonya.kiyatkina@mail.ru>
2023-01-08 06:51:37 +0500

    Заполнение html страницы

--- vacancies_site/skills/views.py
+++ vacancies_site/skills/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import KeySkill
 
 
 def index(request):
-    return render(request, "skills/index.html")
+    context = {'key_skills': KeySkill.objects.all()}
+    return render(request, "skills/index.html", context=context)
