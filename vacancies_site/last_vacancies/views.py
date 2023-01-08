c5baf167811252c0165fa1bcd88c0604928a9dd3
stillouyng <sonya.kiyatkina@mail.ru>
2023-01-08 08:45:14 +0500

    Заполнение html и прописывание стилей

--- vacancies_site/last_vacancies/views.py
+++ vacancies_site/last_vacancies/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .parser import HHParser as hh
 
 
 def index(request):
-    return render(request, "last_vacancies/index.html")
+    vacancies = hh().parse_vacancies()
+    context = {'vacancies': vacancies}
+    return render(request, "last_vacancies/index.html", context=context)
